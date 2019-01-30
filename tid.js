d277087809abcf25c18c962d47313bc96861275c
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-01-30 16:34:11 +0100

    Add Prettier and ESlint, remove xo, format file

--- tid.js
+++ tid.js
@@ -6,7 +6,7 @@
  **/
 (function tid() {
   // Create an array of all the measurements we'll use to display
-  var measure = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year'];
+  var measure = ["second", "minute", "hour", "day", "week", "month", "year"];
   var timeArray = [60, 60, 24, 7, 365 / 7 / 12, 12];
   var timeArrayLen = 6;
 
@@ -28,21 +28,21 @@
 
     var unit = measure[parseInt(i / 2, 10)];
     if (diff > 1) {
-      unit += 's';
+      unit += "s";
     }
 
-    return diff + ' ' + unit + ' ago';
+    return diff + " " + unit + " ago";
   }
 
   // Loop over every <time> element and replace the content of it with the time
   // since the content was posted
-  var elements = document.getElementsByTagName('time');
+  var elements = document.getElementsByTagName("time");
   for (var i = 0; i < elements.length; i++) {
     var el = elements[i];
     // Make some pretty stupid assumptions about the input, it should be a
     // parsable date format for JavaScript, i.e. ISO8601 or RFC2822 and therefore
     // not require any special tinkering
-    var date = Date.parse(el.getAttribute('datetime'));
+    var date = Date.parse(el.getAttribute("datetime"));
     // Get the time difference in seconds since whatever content was posted
     // online and format it so it can be output to our site
     var output = formatTime((new Date() - date) / 1000);
