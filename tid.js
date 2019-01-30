bd70dad4e2035e6ed52cc7d7d0b3dd99747735ba
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-01-30 19:00:53 +0100

    Update script to be even smaller!

--- tid.js
+++ tid.js
@@ -1,44 +1,39 @@
 /**
  * tid.js -- a tiny and stupidly simple relative time ago library
  * MIT License (c) 2016 Sondre Nilsen
- * Version: 1.1.0
+ * Version: 1.2.0
  * https://tid.eons.io
  **/
 (function tid() {
-  // Create an array of all the measurements we'll use to display
-  var measure = ["second", "minute", "hour", "day", "week", "month", "year"];
-  var timeArray = [60, 60, 24, 7, 365 / 7 / 12, 12];
-  var timeArrayLen = 6;
+  // All the units, their time and the result
+  var unit,
+    time = {
+      second: 60,
+      minute: 60,
+      hour: 24,
+      day: 7,
+      week: 4.35,
+      month: 12,
+      year: 12
+    },
+    result;
 
   // Black fucking magic
-  function formatTime(diff) {
-    var i = 0;
-    diff = Math.abs(diff);
+  function formatTime(date) {
+    val = 0 | date;
 
-    for (; diff >= timeArray[i] && i < timeArrayLen; i++) {
-      diff /= timeArray[i];
+    for (unit in time) {
+      result = val % time[unit];
+      if (!(val = 0 | (val / time[unit]))) {
+        return result + " " + (result - 1 ? unit + "s" : unit) + " ago";
+      }
     }
-
-    diff = parseInt(diff, 10);
-    i *= 2;
-
-    if (diff > (i === 0 ? 9 : 1)) {
-      i += 1;
-    }
-
-    var unit = measure[parseInt(i / 2, 10)];
-    if (diff > 1) {
-      unit += "s";
-    }
-
-    return diff + " " + unit + " ago";
   }
 
   // Loop over every <time> element and replace the content of it with the time
   // since the content was posted
   var elements = document.getElementsByTagName("time");
-  for (var i = 0; i < elements.length; i++) {
-    var el = elements[i];
+  for (let el of elements) {
     // Make some pretty stupid assumptions about the input, it should be a
     // parsable date format for JavaScript, i.e. ISO8601 or RFC2822 and therefore
     // not require any special tinkering
